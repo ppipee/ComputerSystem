d2b3b4e827f5552a09a695930377966e52683322
ppipee <kittapon.j@ku.th>
2019-10-12 22:55:04 +0700

    lab4 part1 done (pipeline, don't have forwarding)

---
+++ lab4/6010504651_kittapon_lab4_part1/no_hazard_mips.asm
@@ -0,0 +1,97 @@
+addiu $t0, $zero, 5
+addiu $t1, $zero, 10
+nop
+nop
+addiu $t0, $t0, 5 # $t0 = 10
+addiu $t1, $t1, 10 # $t1 = 20
+nop
+nop
+nop
+addu $t0, $t0, $t1 # t0 = 30
+nop
+nop
+nop
+sw $t0, 0($zero)
+nop
+nop
+lw $t2, 0($zero) # t2 = 30
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 35
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 40
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 45
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 50
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 55
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 60
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 65
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 70
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 75
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 80
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 85
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 90
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 95
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 100
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 105
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 110
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 115
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 120
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 125
+nop
+nop
+nop
+addiu $t2, $t2, 5 # $t2 = 130
